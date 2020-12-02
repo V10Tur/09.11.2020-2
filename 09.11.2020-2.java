adbc5884058547f5d1a826811340c8f17fcfc3af
V10Tur <72814308+V10Tur@users.noreply.github.com>
2020-12-02 11:35:30 +0300

    Create 09.11.2020-2.java

---
+++ 09.11.2020-2.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        System.out.println(sum(2, 3));
+        System.out.println(sum(4, 3));
+    }
+    static int sum(int x, int y){
+
+        return x + y;
+    }
+    static double sum(int x, int y){
+
+        return x + y;
+    }
+
+}
